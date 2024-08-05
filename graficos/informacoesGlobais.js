0e47cba8a1d1fca662d69d561d340aa655b6d81c
jackealura2025 <jackeline.padua@alura.com.br>
2024-08-05 16:48:25 -0300

    aula09

--- graficos/informacoesGlobais.js
+++ graficos/informacoesGlobais.js
@@ -1,7 +1,5 @@
 const url = 'https://raw.githubusercontent.com/guilhermeonrails/api/main/dados-globais.json'
 
-const url = 'https://raw.githubusercontent.com/guilhermeonrails/api/main/dados-globais.json'
-
 async function vizualizarInformacoesGlobais() {
     const res = await fetch(url)
     const dados = await res.json()
